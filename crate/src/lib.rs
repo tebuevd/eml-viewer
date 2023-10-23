5e269d12421a8fb245d92b5b6abb51f01738fb93
dinislam tebuev <3241533+tebuevd@users.noreply.github.com>
2023-10-23 11:09:16 -0700

    added in the wasm

---
+++ crate/src/lib.rs
@@ -0,0 +1,36 @@
+use json;
+use mail_parser::*;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn eml_to_json(data: &[u8]) -> String {
+    let message = MessageParser::default().parse(data).unwrap();
+
+    let to: Vec<_> = message
+        .to()
+        .unwrap()
+        .iter()
+        .map(|x| x.address().unwrap())
+        .collect();
+    let from: Vec<_> = message
+        .from()
+        .unwrap()
+        .iter()
+        .map(|x| x.address().unwrap())
+        .collect();
+    let subject = message.subject().unwrap();
+    let attachment_count = message.attachment_count();
+    let body = message.body_text(0).unwrap();
+    let body_html = message.body_html(0).unwrap();
+
+    let mut json_obj = json::JsonValue::new_object();
+
+    json_obj["to"] = to.into();
+    json_obj["from"] = from.into();
+    json_obj["subject"] = subject.into();
+    json_obj["attachment_count"] = attachment_count.into();
+    json_obj["body"] = body.to_string().into();
+    json_obj["body_html"] = body_html.to_string().into();
+
+    return json_obj.to_string();
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
+import load, { eml_to_json } from "../crate/pkg/eml_viewer";
+
+await load();
+
 function App() {
-  return <main className="p-4 sm:p-6 md:p-8">My Vite App</main>;
+  return (
+    <main className="p-4 sm:p-6 md:p-8">
+      <input
+        type="file"
+        onChange={async (e) => {
+          const file = e.target.files?.[0];
+
+          if (file) {
+            const reader = new FileReader();
+            reader.onload = async (e) => {
+              const contents = e.target?.result as ArrayBuffer;
+              const byteArray = new Uint8Array(contents);
+              const json = eml_to_json(byteArray);
+              console.log(json);
+            };
+            reader.readAsArrayBuffer(file);
+          }
+        }}
+      />
+    </main>
+  );
 }
 
 export default App;
