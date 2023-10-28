747c3f079de2b328dbe454d1d80a2128849b28df
dinislam tebuev <3241533+tebuevd@users.noreply.github.com>
2023-10-28 16:07:25 -0700

    prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,51 @@
 import { useState } from "react";
 import { useFiles } from "./hooks/useFiles";
 import { instance } from "./workers/opfs";
+import { Dropzone } from "./components/dropzone";
 
 function App() {
-  const [json, setJson] = useState<null | Record<
-    string,
-    string | number | string[]
-  >>(null);
-  const { files, addFile } = useFiles();
+	const [json, setJson] = useState<null | Record<
+		string,
+		string | number | string[]
+	>>(null);
+	const { files, addFile } = useFiles();
 
-  console.log(json);
+	return (
+		<main className="grid min-h-full grid-cols-4 grid-rows-5 gap-4">
+			<section
+				id="files"
+				className="col-span-1 col-start-1 row-span-4 row-start-1"
+			>
+				<ul className="mt-4 cursor-pointer space-y-1 text-sm">
+					<li className="cursor-default">🏠 (Root)</li>
+					{files.map((name) => (
+						<li
+							key={name}
+							className="overflow-hidden overflow-ellipsis whitespace-nowrap bg-slate-200 pl-4 hover:bg-transparent"
+							onClick={async () => {
+								const json = await instance.parseEmlFile(name);
+								setJson(json);
+							}}
+						>
+							{name}
+						</li>
+					))}
+				</ul>
+			</section>
+			<section
+				id="dropzone"
+				className="col-span-1 col-start-1 row-span-1 row-end-6 p-1"
+			>
+				<Dropzone className="h-full" />
+			</section>
 
-  return (
-    <main className="grid grid-cols-4 gap-4 min-h-full">
-      <section className="cols-span-1 p-4 pr-0 max-w-xs">
-        <label className="block border-2 border-solid p-2 rounded">
-          <input
-            id="file-input"
-            type="file"
-            className="block w-full text-sm"
-            onChange={async (e) => {
-              const file = e.target.files?.[0];
-
-              if (!file) {
-                return;
-              }
-
-              await addFile(file);
-            }}
-          />
-        </label>
-        <ul className="mt-4 space-y-1 text-sm cursor-pointer">
-          <li className="cursor-default">🏠 (Root)</li>
-          {files.map((name) => (
-            <li
-              key={name}
-              className="pl-4 bg-slate-200 hover:bg-transparent overflow-ellipsis overflow-hidden whitespace-nowrap"
-              onClick={async () => {
-                const json = await instance.parseEmlFile(name);
-                setJson(json);
-              }}
-            >
-              {name}
-            </li>
-          ))}
-        </ul>
-      </section>
-
-      <section className="p-8 pl-0 col-span-3">
-        {json && (
-          <iframe className="w-full h-full" srcDoc={json.body_html as string} />
-        )}
-      </section>
-    </main>
-  );
+			<section className="col-span-3 row-span-full p-8 pl-0">
+				{json && (
+					<iframe className="h-full w-full" srcDoc={json.body_html as string} />
+				)}
+			</section>
+		</main>
+	);
 }
 
 export default App;
