e03cf2f9806f945ce7edc0298471d64678771dad
dinislam tebuev <3241533+tebuevd@users.noreply.github.com>
2023-11-18 10:29:19 -0800

    fix'

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-const root = document.getElementById("div");
+const root = document.getElementById("root");
 
 if (root) {
 	ReactDOM.createRoot(root).render(
