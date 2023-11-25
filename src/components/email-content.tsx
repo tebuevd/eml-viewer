f3c12db99ceb77a8cee83b5bd7678142e6ff946a
dinislam tebuev <3241533+tebuevd@users.noreply.github.com>
2023-11-25 11:48:28 -0800

    remove onscroll handlers

--- src/components/email-content.tsx
+++ src/components/email-content.tsx
@@ -27,30 +27,12 @@ export function EmailContent({ emailHtml, emailId }: EmailContentProps) {
 	}, []);
 
 	return (
-		<div
-			className="isolate flex h-full"
-			onScroll={(e) => {
-				e.stopPropagation();
-				console.log("Scrolling on higher div", e);
-			}}
-		>
+		<div className="isolate flex h-full">
 			<Link className="hidden" id="back" ref={link} to="/" />
 			<aside className="w-40 bg-gradient-to-l from-sh-sidebar-start from-0% to-sh-sidebar-end to-40%" />
 
-			<div
-				className="z-0 flex flex-1 justify-center pb-4 shadow-2xl shadow-black/10"
-				onScroll={(e) => {
-					e.stopPropagation();
-					console.log("Scrolling on div", e);
-				}}
-			>
-				<article
-					className="relative mx-4 mt-20 min-w-[460px] max-w-3xl flex-1 overflow-hidden rounded shadow-2xl shadow-black/50"
-					onScroll={(e) => {
-						e.stopPropagation();
-						console.log("Scrolling on article", e);
-					}}
-				>
+			<div className="z-0 flex flex-1 justify-center pb-4 shadow-2xl shadow-black/10">
+				<article className="relative mx-4 mt-20 min-w-[460px] max-w-3xl flex-1 overflow-hidden rounded shadow-2xl shadow-black/50">
 					<div className="absolute bottom-0 left-0 top-0 z-10 w-[3px] bg-sh-highlightbar" />
 					<IframeRenderer key={emailId} srcDoc={emailHtml} />
 				</article>
